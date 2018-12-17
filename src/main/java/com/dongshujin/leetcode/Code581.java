fe4170979201cde45bf096b64970493784f81419
dongshujin <dongshujin.beans@gmail.com>
2018-12-17 13:59:34 +0800

    Change: 581

--- src/main/java/com/dongshujin/leetcode/Code581.java
+++ src/main/java/com/dongshujin/leetcode/Code581.java
@@ -1,11 +1,24 @@
 package com.dongshujin.leetcode;
 
+import java.util.Stack;
+
 public class Code581 {
 
     public int findUnsortedSubarray(int[] nums) {
-
-        // todo
-        return 0;
+        Stack< Integer > stack = new Stack < Integer > ();
+        int l = nums.length, r = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] > nums[i])
+                l = Math.min(l, stack.pop());
+            stack.push(i);
+        }
+        stack.clear();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i])
+                r = Math.max(r, stack.pop());
+            stack.push(i);
+        }
+        return r - l > 0 ? r - l + 1 : 0;
     }
 
 }
